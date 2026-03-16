2033591d5b9fb56120bd67a14ee6a005261a343a
Quentin Roger <qroger@bouyguestelecom-solution.fr>
2026-03-16 17:42:09 +0100

    feat: restructure database initialization for improved error handling and clarity

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,9 +2,14 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { building } from '$app/environment';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+let db: ReturnType<typeof drizzle<typeof schema>>;
 
-const client = postgres(env.DATABASE_URL);
+if (!building) {
+	if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+	const client = postgres(env.DATABASE_URL);
+	db = drizzle(client, { schema });
+}
 
-export const db = drizzle(client, { schema });
+export { db };
